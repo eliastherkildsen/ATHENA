beae9ffd956f54f2cf40dcc11caa6a02d14102ce
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-27 11:28:01 +0200

    Feature(BookingInformation): started work on bookingInformationController

--- src/main/java/org/apollo/template/Controller/BookingInformationController.java
+++ src/main/java/org/apollo/template/Controller/BookingInformationController.java
@@ -1,16 +1,43 @@
 package org.apollo.template.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import org.apollo.template.model.MeetingType;
+import org.apollo.template.Model.MeetingType;
+import org.apollo.template.persistence.DAO;
+import org.apollo.template.persistence.MeetingTypeDAO.MeetingTypeDBDAO;
 
-public class BookingInformationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookingInformationController implements Initializable {
 
     @FXML
     private TextField textField_name, textField_email, textfield_numberOfParticipants;
 
     @FXML
     private ChoiceBox<MeetingType> choiceBox_meetingType;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadMeetingTypeCB();
+    }
+
+    /**
+     * Method for loading meeting types into choiceBox_meetingType.
+     */
+    private void loadMeetingTypeCB(){
+
+        // creating instance of dao.
+        DAO<MeetingType> dao = new MeetingTypeDBDAO();
+
+        // clearing the content of choiceBox to avoid replica data.
+        choiceBox_meetingType.getItems().clear();
+
+        // loading meeting types from dao into choiceBox.
+        choiceBox_meetingType.getItems().addAll(dao.readAll());
+    }
+
 
 }
