f196a00cfa77d3b5b1049765fdef2125d56e77e7
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-05-27 14:43:50 +0200

    feat(AvailableComponent): Started programming of custom component

--- src/main/java/org/apollo/template/Controller/AvailableRoomsController.java
+++ src/main/java/org/apollo/template/Controller/AvailableRoomsController.java
@@ -1,4 +1,13 @@
 package org.apollo.template.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import org.apollo.template.View.UI.AvailableComponent;
+
 public class AvailableRoomsController {
+
+    @FXML
+    private VBox Vbox_Listview;
+
+    AvailableComponent availableComponent = new AvailableComponent("301", "3. sal", 25, "Projektor, Højtaler, Airtame");
 }
--- src/main/java/org/apollo/template/View/UI/AvailableComponent.java
+++ src/main/java/org/apollo/template/View/UI/AvailableComponent.java
@@ -1,9 +1,80 @@
 package org.apollo.template.View.UI;
 
-public class AvailableComponent {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
-    public AvailableComponent(){
+public class AvailableComponent extends HBox {
 
+
+
+    public AvailableComponent(String roomNo, String floor, int personKapacity, String inventory){
+
+        Label label_roomNo = buildLabel(roomNo, 18, FontWeight.BOLD);
+        Label label_floor = buildLabel(floor, 18, FontWeight.NORMAL);
+        Label label_personKap = buildLabelWithInt(personKapacity, 18, FontWeight.NORMAL);
 
     }
+
+    private Label buildLabel(String text, int fontSize, FontWeight fontWeight) {
+        Label label = new Label(text);
+        label.setFont(Font.font("System", fontWeight, FontPosture.REGULAR, fontSize));
+        this.setHgrow(label, Priority.ALWAYS);
+        this.setMargin(label, new Insets(0,20,0,20));
+        return label;
+    }
+
+    private Label buildLabelWithInt(int number, int fontSize, FontWeight fontWeight) {
+
+        String kapacity = String.valueOf(number);
+
+        Label label = new Label(kapacity);
+        label.setFont(Font.font("System", fontWeight, FontPosture.REGULAR, fontSize));
+        this.setHgrow(label, Priority.ALWAYS);
+        this.setMargin(label, new Insets(0,20,0,20));
+        return label;
+    }
+
+
+
+
+    private void forNow(){
+        Label label_availableRoom = new Label();
+        label_availableRoom.setMaxHeight(77.5);
+        label_availableRoom.setMaxWidth(600);
+
+        getChildren().addAll(label_availableRoom);
+
+
+
+        VBox vbox = new VBox();
+
+
+
+        HBox hBox_roomInfo = new HBox();
+        hBox_roomInfo.setAlignment(Pos.CENTER_LEFT);
+
+        HBox hBox_inventoryInfo = new HBox();
+        hBox_inventoryInfo.setAlignment(Pos.CENTER_LEFT);
+
+        vbox.getChildren().addAll(hBox_roomInfo,hBox_inventoryInfo);
+
+
+        Label label_localNo = new Label();
+        Label label_floor = new Label();
+        Label label_personKapacity = new Label();
+
+        hBox_roomInfo.getChildren().addAll(label_localNo, label_floor, label_personKapacity);
+
+        Label label_inventory = new Label();
+        hBox_inventoryInfo.getChildren().addAll(label_inventory);
+    }
+
+
 }
