a91d6d87e0be4f5c855d1909353898133df470c7
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-27 10:13:29 +0200

    Feature(BookingInformation): added refferences to fxmlview in controller

--- src/main/java/org/apollo/template/Controller/BookingInformationController.java
+++ src/main/java/org/apollo/template/Controller/BookingInformationController.java
@@ -1,4 +1,16 @@
 package org.apollo.template.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import org.apollo.template.model.MeetingType;
+
 public class BookingInformationController {
+
+    @FXML
+    private TextField textField_name, textField_email, textfield_numberOfParticipants;
+
+    @FXML
+    private ChoiceBox<MeetingType> choiceBox_meetingType;
+
 }
