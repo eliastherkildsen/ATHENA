38a344c738d8894acbd051bd52bf31d79a8fbf46
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-27 09:20:28 +0200

    Feature(SystemChose): added refferences to fxmlview in controller

--- src/main/java/org/apollo/template/Controller/SystemChoseController.java
+++ src/main/java/org/apollo/template/Controller/SystemChoseController.java
@@ -1,7 +1,29 @@
 package org.apollo.template.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+/*
+    This class contains logic for chosing witch part of the system the user wants
+    to use.
+ */
 public class SystemChoseController {
 
-    
+    @FXML
+    private Button button_admin, button_infoScreen;
+
+
+    @FXML
+    protected void onButton_adminClick(){
+
+    }
+
+    @FXML
+    protected void onButton_infoScreen(){
+
+    }
+
+
 
 }
