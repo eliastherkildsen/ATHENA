88046973e0391b57cd72e917d278e180407d90f1
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-27 11:27:25 +0200

    Feature(BookingInformation): created meetingType obj

---
+++ src/main/java/org/apollo/template/Model/MeetingType.java
@@ -0,0 +1,28 @@
+package org.apollo.template.Model;
+
+
+/*
+    Object representation of meeting type.
+ */
+public class MeetingType {
+
+    private final String MEETING_TYPE;
+    private int meetingTypeID;
+
+    public MeetingType(String meetingType) {
+        this.MEETING_TYPE = meetingType;
+    }
+
+    public MeetingType(String meetingType, int meetingTypeID) {
+        this.MEETING_TYPE = meetingType;
+        this.meetingTypeID = meetingTypeID;
+    }
+
+    public int getMeetingTypeID() {
+        return meetingTypeID;
+    }
+
+    public String getMeetingType() {
+        return MEETING_TYPE;
+    }
+}
