ab84ae8c5ed3b5dd2c419ab4a48940cdf18b311b
Bolt404 <mads@rknudsen.dk>
2024-05-27 14:41:10 +0200

    feat(BookingInformationDAO): First implementation of DAO

--- src/main/java/org/apollo/template/View/ViewList.java
+++ src/main/java/org/apollo/template/View/ViewList.java
@@ -23,7 +23,7 @@ public enum ViewList {
     /**
      * Info view.
      */
-    INFOSCREEN("InfoView.fxml");
+    INFOSCREEN("InfoView.fxml"),
 
     /**
      *  Topbar view
---
+++ src/main/java/org/apollo/template/persistence/BookingInformationDAO.java
@@ -0,0 +1,48 @@
+package org.apollo.template.persistence;
+
+import org.apollo.template.Model.BookingInformation;
+
+import java.util.List;
+
+public class BookingInformationDAO implements DAO<BookingInformation>{
+
+    @Override
+    public void add(BookingInformation bookingInformation) {
+
+    }
+
+    @Override
+    public void addAll(List<BookingInformation> list) {
+
+    }
+
+    @Override
+    public void delete(BookingInformation bookingInformation) {
+
+    }
+
+    @Override
+    public void deleteAll(List<BookingInformation> list) {
+
+    }
+
+    @Override
+    public void update(BookingInformation bookingInformation) {
+
+    }
+
+    @Override
+    public void updateAll(List<BookingInformation> t) {
+
+    }
+
+    @Override
+    public BookingInformation read(int id) {
+        return null;
+    }
+
+    @Override
+    public List<BookingInformation> readAll() {
+        return List.of();
+    }
+}
