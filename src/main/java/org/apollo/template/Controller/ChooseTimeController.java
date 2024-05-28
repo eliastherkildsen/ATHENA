b41d17306bdb35ab5d75eefec90e9fbed0d921cb
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2024-05-28 08:52:56 +0200

    feat(ChoosenTime): Generate times method

--- src/main/java/org/apollo/template/Controller/ChooseTimeController.java
+++ src/main/java/org/apollo/template/Controller/ChooseTimeController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ChooseTimeController implements Initializable {
@@ -14,23 +18,35 @@ public class ChooseTimeController implements Initializable {
     @FXML
     private GridPane gridPane_ButtonGrid;
     @FXML
-    private Label label_StartTime;
+    private Label label_StartTime, label_EndTime;
 
     private int cnt, noOfButtons = 32;
-
-    private int hour = 8, minute = 0;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        List<String> times = generateTimes(noOfButtons);
 
         while (cnt < noOfButtons){
 
-            Button button = new Button("Button " + (cnt + 1));
+            Button button = new Button(times.get(cnt));
             button.setPrefSize(114, 42);
             button.setOnAction(event -> {
-                label_StartTime.setText(button.getText());
+
+                if (label_StartTime.getText().isEmpty()){
+                    label_StartTime.setText(button.getText());
+
+                } else if (label_EndTime.getText().isEmpty() &&
+                        label_StartTime.hashCode() > button.getText().hashCode()){
+                    label_EndTime.setText(button.getText());
+
+                } else {
+                    label_EndTime.setText(label_StartTime.getText());
+                    label_StartTime.setText(button.getText());
+                }
+
+
             });
 
             gridPane_ButtonGrid.add(button, cnt%4, cnt/4);
@@ -40,5 +56,23 @@ public class ChooseTimeController implements Initializable {
         }
 
 
+    }
+
+    /**
+     * A Method for generating all the time slots that need to be given to the different buttons in the grid
+     * @param numberOfTimes How many times the method shoe generate
+     * @return Returns a list of all the generated times
+     */
+    private List<String> generateTimes(int numberOfTimes){
+
+        List<String> times = new ArrayList<>();
+        LocalTime startTime = LocalTime.of(8,0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        for (int i = 0; i < numberOfTimes; i++) {
+            times.add(startTime.plusMinutes(i * 15).format(formatter));
+        }
+
+        return times;
     }
 }
