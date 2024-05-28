0035fadaa0fc2517de268904740634583ebbf4bb
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-05-28 19:55:46 +0200

    refactor(databasseInsert.sql): refactored stored procedure "getAvailableRooms"

--- src/main/java/org/apollo/template/Database/databasseInsert.sql
+++ src/main/java/org/apollo/template/Database/databasseInsert.sql
@@ -175,7 +175,7 @@ CREATE PROCEDURE getAvailableRooms (@BookingDate DATE)
 AS
 BEGIN
 
-    -- Calculates the total booking time for each room on the specified date
+    -- Calculates the total booking time for each room on the specified date ('NULL' if no booking yet)
     WITH BookedTime AS (
         SELECT
             fld_roomID,
@@ -188,33 +188,31 @@ BEGIN
             fld_roomID
     )
 
-    -- Selects selected fields from 4 tables related to each other using INNER JOIN - these are to be presented when displaying available rooms
+    -- Selects selected fields from 3 tables related to each other using INNER JOIN and LEFT JOIN - these are to be presented when displaying available rooms
     SELECT
-        tbl_room.fld_roomID,
         tbl_room.fld_roomName,
         tbl_room.fld_floor,
         tbl_room.fld_roomMaxPersonCount,
-        tbl_booking.fld_bookingID,
-        tbl_inventory.fld_inventoryName,
+        tbl_booking.fld_date,
         tbl_roomType.fld_roomTypeName,
         BookedTime.total_booked_time_minutes
+
     FROM
         tbl_room
-            INNER JOIN tbl_booking ON tbl_room.fld_roomID = tbl_booking.fld_roomID
-            INNER JOIN tbl_roomInventory ON tbl_room.fld_roomID = tbl_roomInventory.fld_roomID
-            INNER JOIN tbl_inventory ON tbl_roomInventory.fld_roomInventoryID = tbl_inventory.fld_inventoryID
+            LEFT JOIN tbl_booking ON tbl_room.fld_roomID = tbl_booking.fld_roomID AND tbl_booking.fld_date = @BookingDate
             INNER JOIN tbl_roomType ON tbl_room.fld_roomTypeID = tbl_roomType.fld_roomTypeID
             LEFT JOIN BookedTime ON tbl_room.fld_roomID = BookedTime.fld_roomID
 
     -- Filters the results to include only rooms that are either not booked on the specified date or have a total booked time less than 8 hours (480 minutes)
     WHERE
-        fld_date = @BookingDate
-    AND COALESCE(BookedTime.total_booked_time_minutes, 0) < 480
+        BookedTime.total_booked_time_minutes < 480 OR BookedTime.total_booked_time_minutes IS NULL
 
     -- The available rooms are sorted by room name (ascending) and start time (ascending)
     ORDER BY
         tbl_room.fld_roomName, tbl_booking.fld_startTime ASC;
+
 END;
+
 
 
 
