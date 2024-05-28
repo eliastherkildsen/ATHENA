fbc2e242b25e72dbe1774640544d4ae9e872f227
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2024-05-28 10:10:25 +0200

    feat(ChoosenTime): Select time logic

--- src/main/java/org/apollo/template/Controller/ChooseTimeController.java
+++ src/main/java/org/apollo/template/Controller/ChooseTimeController.java
@@ -20,7 +20,9 @@ public class ChooseTimeController implements Initializable {
     @FXML
     private Label label_StartTime, label_EndTime;
 
-    private int cnt, noOfButtons = 32;
+    private int cnt, noOfButtons = 32, institutionInterval = 15;
+
+    private List<Button> buttonList = new ArrayList<>();
 
 
     @Override
@@ -31,20 +33,29 @@ public class ChooseTimeController implements Initializable {
         while (cnt < noOfButtons){
 
             Button button = new Button(times.get(cnt));
+            buttonList.add(button);
             button.setPrefSize(114, 42);
             button.setOnAction(event -> {
 
-                if (label_StartTime.getText().isEmpty()){
-                    label_StartTime.setText(button.getText());
+                String selectedTime = button.getText();
+                handleTimeSelection(selectedTime);
 
-                } else if (label_EndTime.getText().isEmpty() &&
-                        label_StartTime.hashCode() > button.getText().hashCode()){
-                    label_EndTime.setText(button.getText());
-
-                } else {
-                    label_EndTime.setText(label_StartTime.getText());
-                    label_StartTime.setText(button.getText());
-                }
+//                if (label_StartTime.getText().isEmpty()){
+//                    label_StartTime.setText(button.getText());
+//
+//                } else if (label_StartTime.getText().hashCode() < button.getText().hashCode()){
+//                    label_EndTime.setText(button.getText());
+//
+//                } else if (label_EndTime.getText().isEmpty() ){
+//                    label_EndTime.setText(label_StartTime.getText());
+//                    label_StartTime.setText(button.getText());
+//
+//                } else if (label_StartTime.getText().hashCode() > button.getText().hashCode()){
+//                    label_StartTime.setText(button.getText());
+//
+//                } else if (label_EndTime.getText().hashCode() < button.getText().hashCode()){
+//                    label_EndTime.setText(button.getText());
+//                }
 
 
             });
@@ -54,7 +65,46 @@ public class ChooseTimeController implements Initializable {
             cnt++;
 
         }
+    }
 
+    /**
+     * Method for handling all the logic for the buttons being able to select and switch the chosen times
+     * @param selectedTime The time selected by the user
+     */
+    private void handleTimeSelection(String selectedTime){
+
+        LocalTime selectedLocalTime = LocalTime.parse(selectedTime);
+        // All cases where both labels are not filled
+        if (label_StartTime.getText().isEmpty()){
+            label_StartTime.setText(selectedTime);
+
+        } else if (label_EndTime.getText().isEmpty()){
+            if (isEarlier(label_StartTime.getText(), selectedTime)){
+                label_EndTime.setText(selectedTime);
+
+            } else {
+                label_EndTime.setText(label_StartTime.getText());
+                label_StartTime.setText(selectedTime);
+            }
+            // All cases where the labels are filled
+        } else {
+            LocalTime startTime = LocalTime.parse(label_StartTime.getText());
+            LocalTime endTime = LocalTime.parse(label_EndTime.getText());
+
+            if (selectedLocalTime.isBefore(startTime)){
+                label_StartTime.setText(selectedTime);
+
+            } else if (selectedLocalTime.isAfter(endTime)){
+                label_EndTime.setText(selectedTime);
+
+            } else {
+                // If the selected time is between the already chosen times
+                label_StartTime.setText(selectedTime);
+                // If the time in label_StartTime is before the newly selected time, then set label_EndTime as end time if false then set it to startTime
+                label_EndTime.setText(startTime.isBefore(selectedLocalTime) ?
+                        endTime.format(DateTimeFormatter.ofPattern("HH:mm")) : startTime.format(DateTimeFormatter.ofPattern("HH:mm")));
+            }
+        }
 
     }
 
@@ -70,9 +120,22 @@ public class ChooseTimeController implements Initializable {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
         for (int i = 0; i < numberOfTimes; i++) {
-            times.add(startTime.plusMinutes(i * 15).format(formatter));
+            times.add(startTime.plusMinutes(i * institutionInterval).format(formatter));
         }
 
         return times;
+    }
+
+    /**
+     * A method for comparing times in HH:mm format.
+     * @param time1 First time
+     * @param time2 Second time
+     * @return True if time1 is earlier than time2
+     */
+    private boolean isEarlier(String time1, String time2){
+        LocalTime t1 = LocalTime.parse(time1);
+        LocalTime t2 = LocalTime.parse(time2);
+        return t1.isBefore(t2);
+
     }
 }
