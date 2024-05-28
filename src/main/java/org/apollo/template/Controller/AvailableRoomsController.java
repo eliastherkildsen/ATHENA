56b2462672b1f0eadbead40c47bac573c21d5c43
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-05-28 14:51:22 +0200

    feat(AvailableRoom): created new AvailableRoom class + started preparedstatement in AvailableRoomController

--- src/main/java/org/apollo/template/Controller/AvailableRoomsController.java
+++ src/main/java/org/apollo/template/Controller/AvailableRoomsController.java
@@ -3,9 +3,16 @@ package org.apollo.template.Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
+import org.apollo.template.Database.JDBC;
+import org.apollo.template.Model.AvailableRoom;
+import org.apollo.template.Service.Logger.LoggerMessage;
 import org.apollo.template.View.UI.AvailableComponent;
-
 import java.net.URL;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AvailableRoomsController implements Initializable {
@@ -18,9 +25,27 @@ public class AvailableRoomsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // getting today's date
+        Date dateToday = Date.valueOf(LocalDate.now());
 
+        // custom component
         AvailableComponent availableComponent = new AvailableComponent("301", "3. sal", 25, "Projektor, Højtaler, Airtame");
-
         Vbox_Listview.getChildren().add(availableComponent);
+
+        // search for available rooms today's date
+        List<AvailableRoom> availableRooms = storedSqlProcedure(dateToday);
+    }
+
+    private List<AvailableRoom> storedSqlProcedure(Date dateToday) {
+
+        try{
+            PreparedStatement ps = JDBC.get().getConnection().prepareStatement("EXECUTE getAvailableRooms @BookingDate = ?");
+            ps.setDate(1, dateToday);
+
+        }catch (SQLException e){
+            LoggerMessage.error(this,"Stored Procedure : getAvailableRooms didn't run as intended " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 }
