dcc9f6bf76a7c2e87a827943a348f68497f91589
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-29 17:48:37 +0200

    fix(errorReport): added missing datafield fld_reportDescription

--- src/main/java/org/apollo/template/Database/databaseCreateScript.sql
+++ src/main/java/org/apollo/template/Database/databaseCreateScript.sql
@@ -50,6 +50,7 @@ CREATE TABLE tbl_errorReport(
     fld_reportDate DATE NOT NULL,
     fld_inventoryID INTEGER,
     fld_userID INTEGER,
+    fld_reportDescription VARCHAR(264),
 
     FOREIGN KEY (fld_inventoryID) REFERENCES tbl_inventory (fld_inventoryID),
     FOREIGN KEY (fld_userID) REFERENCES tbl_userEmail(fld_userID)
