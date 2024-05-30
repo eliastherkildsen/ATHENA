b77b3bb0c6e4bbccb98ff8247c4a8d73066dceff
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-05-30 08:22:12 +0200

    fix(DAO): made dao not use addAll, RemoveAll, DeleteAll

--- src/main/java/org/apollo/template/persistence/JDBC/DAO/DAO.java
+++ src/main/java/org/apollo/template/persistence/JDBC/DAO/DAO.java
@@ -5,13 +5,10 @@ import java.util.List;
 public interface DAO <T>{
 
     void add(T t);
-    void addAll(List<T> list);
 
     void delete(T t);
-    void deleteAll(List<T> list);
 
     void update(T t);
-    void updateAll(List<T> t);
 
     T read(int id);
     List<T> readAll();
