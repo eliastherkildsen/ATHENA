0c83349c6020276b19af8340b32bdf0da484df20
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-06-04 09:17:17 +0200

    feat(AdminLoginView): created view with css (AdminMenu.css)

--- src/main/java/org/apollo/template/Controller/AdminLoginController.java
+++ src/main/java/org/apollo/template/Controller/AdminLoginController.java
@@ -1,4 +1,10 @@
 package org.apollo.template.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class AdminLoginController {
+
+    @FXML
+    private VBox vBox_login;
 }
