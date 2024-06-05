99f5010b1e8e3546dda4db68330e8b34fed83c13
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-06-05 08:15:49 +0200

    feat(DayStrategy): created class

---
+++ src/main/java/org/apollo/template/Model/Statistics/DayStrategy.java
@@ -0,0 +1,9 @@
+package org.apollo.template.Model.Statistics;
+
+public class DayStrategy implements TimeStrategy{
+
+    @Override
+    public StatObj generateObj(StatisticsArea statisticsArea) {
+        return null;
+    }
+}
