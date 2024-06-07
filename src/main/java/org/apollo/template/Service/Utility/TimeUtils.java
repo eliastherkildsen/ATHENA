8ad24e58c88aa0ce1e55d2120fcf1f7ad37792ad
Bolt404 <mads@rknudsen.dk>
2024-06-07 11:34:14 +0200

    refactor(TimeUtils): Fixed

--- src/main/java/org/apollo/template/Service/Utility/TimeUtils.java
+++ src/main/java/org/apollo/template/Service/Utility/TimeUtils.java
@@ -1,22 +1,16 @@
 package org.apollo.template.Service.Utility;
 
 import org.apollo.template.Model.Booking;
-import org.apollo.template.Service.Logger.LoggerMessage;
-import org.jetbrains.annotations.NotNull;
 
 import java.sql.Time;
 
-public final class TimeUtils {
-    private TimeUtils() {
-        LoggerMessage.warning(TimeUtils.class.getName(), "Utility class - Cannot be instantiated");
-    }
-
+public class TimeUtils {
     /**
      * getStringTimeFormatted takes a Booking object and returns with a formated string as HH:MM - HH:MM
      * @param booking
      * @return String
      */
-    public static @NotNull String getStringTimeFormatted(Booking booking) {
+    public static String getStringTimeFormatted(Booking booking) {
         Time starttime = booking.getStartTime();
         Time endtime = booking.getEndTime();
 
