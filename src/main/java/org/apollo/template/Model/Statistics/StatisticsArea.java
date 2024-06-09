b4577c461b460e6335a545a8f944e918df8ef389
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-06-09 12:30:06 +0200

    refactor(TimeStrategy): refactored "statisticsArea" definition - wasn't correct

--- src/main/java/org/apollo/template/Model/Statistics/StatisticsArea.java
+++ src/main/java/org/apollo/template/Model/Statistics/StatisticsArea.java
@@ -1,6 +1,8 @@
 package org.apollo.template.Model.Statistics;
 
+// Enumerations for defining different areas of statistics
 public enum StatisticsArea {
 
+    // represents statistics related to booking time
     BOOKINGS;
 }
--- src/main/java/org/apollo/template/Model/Statistics/TimeStrategy.java
+++ src/main/java/org/apollo/template/Model/Statistics/TimeStrategy.java
@@ -4,7 +4,7 @@ public interface TimeStrategy {
 
     /**
      * This method generates a StatObj based on a specific strategy and a roomID
-     * @param statisticsArea the strategy (time period) that should be used to generate the object.
+     * @param statisticsArea the area of statistics to be generated
      * @param roomID the roomID for a selected room
      * @return a StatObj that contains the variables needed to create a barChart.
      */
